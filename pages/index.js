801dafee41cd7f06d659e25d5b45e5336866879e
Yusuke Endoh <mame@ruby-lang.org>
2021-03-25 22:06:00 +0900

    Deploy

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+const q = (n) => String.fromCharCode(n);
+const u = [39, 34, 39, 36, 32, 39, 34, 39].map(q).join("");
+const e = `const q = (n) => String.fromCharCode(n);
+const u = [39, 34, 39, 36, 32, 39, 34, 39].map(q).join("");
+const e = A.replace(u.substr(2, 4), u);
+const d = "echo " + q(39) + e + q(39) + " > pages/index.js";
+const Q = () => (
+  <div style={{ margin: "auto", maxWidth: "40em" }}>
+    <h1>Next.js Quine</h1>
+    <h2>1. Create a nextjs app</h2>
+    <pre>npx create-next-app nextjs-quine</pre>
+    <h2>2. Move into the directory</h2>
+    <pre>cd nextjs-quine</pre>
+    <h2>3. Replace pages/index.js as follows</h2>
+    <pre>{d.replace("A", q(96) + e + q(96))}</pre>
+    <h2>4. Start the app and open http://localhost:3000</h2>
+    <pre>npx next dev</pre>
+    <h2>Source code</h2>
+    <a href="https://github.com/mame/nextjs-quine">github: mame/nextjs-quine</a>
+    <style jsx>{"pre:before{content:'$ ';}"}</style>
+    <style jsx>{"pre{margin:1em 0em;user-select:all;}"}</style>
+  </div>
+);
+export default Q;`.replace(u.substr(2, 4), u);
+const d = "echo " + q(39) + e + q(39) + " > pages/index.js";
+const Q = () => (
+  <div style={{ margin: "auto", maxWidth: "40em" }}>
+    <h1>Next.js Quine</h1>
+    <h2>1. Create a nextjs app</h2>
+    <pre>npx create-next-app nextjs-quine</pre>
+    <h2>2. Move into the directory</h2>
+    <pre>cd nextjs-quine</pre>
+    <h2>3. Replace pages/index.js as follows</h2>
+    <pre>{d.replace("A", q(96) + e + q(96))}</pre>
+    <h2>4. Start the app and open http://localhost:3000</h2>
+    <pre>npx next dev</pre>
+    <h2>Source code</h2>
+    <a href="https://github.com/mame/nextjs-quine">github: mame/nextjs-quine</a>
+    <style jsx>{"pre:before{content:'$ ';}"}</style>
+    <style jsx>{"pre{margin:1em 0em;user-select:all;}"}</style>
+  </div>
+);
+export default Q;
